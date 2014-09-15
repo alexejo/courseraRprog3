23522d4055686900a3a37c1543ca6df7eea1a8ea
Alex <invergarry@gmail.com>
2014-09-15 20:41:42 +0200

    Progressed function development

--- prog3.R
+++ prog3.R
@@ -1,24 +1,25 @@
 best <- function(state,outcome){
     ## Read outcome data
-    outcome <- read.csv("outcome-of-care-measures.csv",colClasses = "character")
+    outcomes.data <- read.csv("outcome-of-care-measures.csv",colClasses = "character")
 
     ## table of illnesses and column numbers for data set
     illness.table <- data.frame(illness=c("heart attack","heart failure","pneumonia"),colnum=c(11,18,27))
-
     ## capture data and
     ## Check that state and outcome are valid
     states.vector <- outcome$State==state
     column.vector <- illness.table[,1]==outcome
+    column.number <- illness.table[column.vector,2]
     
-    if(sum(states.vector))==0{
+    if(sum(states.vector)==0){
         stop("invalid state")
-    }else if(sum(column.vector))==0{
+    }
+    if(sum(column.number)==0){
         stop("invalid outcome")
     }
 
-    column.number <- illness.table[column.vector,2]
+    
 
-    illness.outcomes <- outcomes[states.vector,column.number]
+    illness.outcomes <- outcomes.data[states.vector,column.number]
     ## return hospital name in that state with lowest 30 day
     ## death rate
 
