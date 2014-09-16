8ac8cbc09e46f89c2d9ff30d2fff85b91f37e60d
Alex <invergarry@gmail.com>
2014-09-16 10:36:03 +0200

    further update for part 2

--- prog3.R
+++ prog3.R
@@ -3,10 +3,10 @@ best <- function(state,outcome){
     outcomes.data <- read.csv("outcome-of-care-measures.csv",colClasses = "character")
 
     ## table of illnesses and column numbers for data set
-    illness.table <- data.frame(illness=c("heart attack","heart failure","pneumonia"),colnum=c(11,18,27))
+    illness.table <- data.frame(illness=c("heart attack","heart failure","pneumonia"),colnum=c(11,17,23))
     ## capture data and
     ## Check that state and outcome are valid
-    states.vector <- outcome$State==state
+    states.vector <- outcomes.data$State==state
     column.vector <- illness.table[,1]==outcome
     column.number <- illness.table[column.vector,2]
     
@@ -19,8 +19,25 @@ best <- function(state,outcome){
 
     
 
-    illness.outcomes <- outcomes.data[states.vector,column.number]
+    illness.outcomes <- outcomes.data[states.vector,c(2,column.number)]
     ## return hospital name in that state with lowest 30 day
+
     ## death rate
+    illness.outcomes[,2] <- as.numeric(illness.outcomes[,2])
+
+    ##remove nas
+    illness.outcomes <- illness.outcomes[!is.na(illness.outcomes[,2]),]
+    
+    lowest.mortality <- min(illness.outcomes[,2])
+
+    ## find hospital(s) with lowest mortality
+    low.mort.hosp <- illness.outcomes[illness.outcomes[,2]==lowest.mortality,1]
+
+    ## find number of hospitals and if so sort and find first alphabetically
+    if (length(low.mort.hosp)>1){
+        low.mort.hosp <- as.character(sort(low.mort.hosp))        
+    }
+
+    low.mort.hosp[1]
 
 }
